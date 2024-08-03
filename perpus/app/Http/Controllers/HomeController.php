8a86800f10035a3fc1cdbbb17018d867a2cc4574
muhammadega21 <dermawane988@gmail.com>
2024-08-03 15:14:48 +0700

    done

--- perpus/app/Http/Controllers/HomeController.php
+++ perpus/app/Http/Controllers/HomeController.php
@@ -17,12 +17,114 @@ class HomeController extends Controller
         if (Auth()->user()->role === 3) {
             $peminjaman = Peminjaman::where('siswa_id', Auth()->user()->siswa->id)->get();
             $denda = Denda::where('siswa_id', Auth()->user()->siswa->id)->get();
-            $pengembalian = Pengembalian::where('siswa_id', Auth()->user()->siswa->id)->get();
+            $pengembalian = Buku::where('siswa_id', Auth()->user()->siswa->id)->get();
         } else {
             $peminjaman = Peminjaman::all();
             $denda = Denda::all();
-            $pengembalian = Pengembalian::all();
+            $pengembalian = Buku::all();
         }
+        // Chart
+        $monthNow = date('m');
+        $chartLabels = [];
+        $chartDataSets = [];
+        if ($monthNow <= 6) {
+            $chartLabels = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli'];
+            $chartDataSets = [
+                [
+                    'label' => 'Peminjaman',
+                    'data' => [
+                        Peminjaman::whereMonth('created_at', 1)->get()->count(),
+                        Peminjaman::whereMonth('created_at', 2)->get()->count(),
+                        Peminjaman::whereMonth('created_at', 3)->get()->count(),
+                        Peminjaman::whereMonth('created_at', 4)->get()->count(),
+                        Peminjaman::whereMonth('created_at', 5)->get()->count(),
+                        Peminjaman::whereMonth('created_at', 6)->get()->count(),
+                    ],
+                    'fill' => false,
+                    'borderColor' => 'rgb(75, 192, 192)',
+                    'tension' => (int) 0.1
+                ],
+                [
+                    'label' => 'Buku Masuk',
+                    'data' => [
+                        Buku::whereMonth('created_at', 1)->get()->count(),
+                        Buku::whereMonth('created_at', 2)->get()->count(),
+                        Buku::whereMonth('created_at', 3)->get()->count(),
+                        Buku::whereMonth('created_at', 4)->get()->count(),
+                        Buku::whereMonth('created_at', 5)->get()->count(),
+                        Buku::whereMonth('created_at', 6)->get()->count(),
+                    ],
+                    'fill' => false,
+                    'borderColor' => 'rgb(38, 232, 35)',
+                    'tension' => (int) 0.1
+                ],
+                [
+                    'label' => 'Siswa',
+                    'data' => [
+                        Siswa::whereMonth('created_at', 1)->get()->count(),
+                        Siswa::whereMonth('created_at', 2)->get()->count(),
+                        Siswa::whereMonth('created_at', 3)->get()->count(),
+                        Siswa::whereMonth('created_at', 4)->get()->count(),
+                        Siswa::whereMonth('created_at', 5)->get()->count(),
+                        Siswa::whereMonth('created_at', 6)->get()->count(),
+                    ],
+                    'fill' => false,
+                    'borderColor' => 'rgb(222, 121, 7)',
+                    'tension' => (int) 0.1
+                ]
+            ];
+        } else {
+            $chartLabels = ['Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
+            $chartDataSets = [
+                [
+                    'label' => 'Peminjaman',
+                    'data' => [
+                        Peminjaman::whereMonth('created_at', 7)->get()->count(),
+                        Peminjaman::whereMonth('created_at', 8)->get()->count(),
+                        Peminjaman::whereMonth('created_at', 9)->get()->count(),
+                        Peminjaman::whereMonth('created_at', 10)->get()->count(),
+                        Peminjaman::whereMonth('created_at', 11)->get()->count(),
+                        Peminjaman::whereMonth('created_at', 12)->get()->count(),
+                    ],
+                    'fill' => false,
+                    'borderColor' => 'rgb(75, 192, 192)',
+                    'tension' => (int) 0.1
+                ],
+                [
+                    'label' => 'Buku Masuk',
+                    'data' => [
+                        Buku::whereMonth('created_at', 7)->get()->count(),
+                        Buku::whereMonth('created_at', 8)->get()->count(),
+                        Buku::whereMonth('created_at', 9)->get()->count(),
+                        Buku::whereMonth('created_at', 10)->get()->count(),
+                        Buku::whereMonth('created_at', 11)->get()->count(),
+                        Buku::whereMonth('created_at', 12)->get()->count(),
+                    ],
+                    'fill' => false,
+                    'borderColor' => 'rgb(38, 232, 35)',
+                    'tension' => (int) 0.1
+                ],
+                [
+                    'label' => 'Siswa',
+                    'data' => [
+                        Siswa::whereMonth('created_at', 7)->get()->count(),
+                        Siswa::whereMonth('created_at', 8)->get()->count(),
+                        Siswa::whereMonth('created_at', 9)->get()->count(),
+                        Siswa::whereMonth('created_at', 10)->get()->count(),
+                        Siswa::whereMonth('created_at', 11)->get()->count(),
+                        Siswa::whereMonth('created_at', 12)->get()->count(),
+                    ],
+                    'fill' => false,
+                    'borderColor' => 'rgb(222, 121, 7)',
+                    'tension' => (int) 0.1
+                ]
+            ];
+        }
+        $chartData = [
+            'labels' => $chartLabels,
+            'datasets' => $chartDataSets
+        ];
+
         return view('/dashboard', [
             'title' => "Dashboard",
             'main_page' => '',
@@ -30,10 +132,11 @@ class HomeController extends Controller
             'siswa' => Siswa::all()->count(),
             'petugas' => Petugas::all()->count(),
             'kelas' => Kelas::all()->count(),
-            'buku' => Buku::all()->count(),
-            'peminjaman' => $peminjaman->count(),
-            'pengembalian' => $pengembalian->count(),
-            'denda' => $denda->count(),
+            'buku' => Buku::all(),
+            'peminjaman' => $peminjaman,
+            'pengembalian' => $pengembalian,
+            'denda' => $denda,
+            'chartData' => $chartData
         ]);
     }
 }
--- perpus/resources/views/dashboard.blade.php
+++ perpus/resources/views/dashboard.blade.php
@@ -7,7 +7,20 @@
             <x-card cardTitle='Petugas' icon='bi-person-badge'>{{ $petugas }}</x-card>
         @endcan
         <x-card cardTitle='Kelas' icon='bi-house'>{{ $kelas }}</x-card>
-        <x-card cardTitle='Buku' icon='bi-book'>{{ $buku }}</x-card>
+    @endcan
+
+    <x-card cardTitle='Buku' icon='bi-book'>{{ $buku->count() }}</x-card>
+    <x-card cardTitle='Denda' icon='bi-exclamation-circle'>{{ $denda->count() }}</x-card>
+    <x-card cardTitle='Peminjaman' icon='bi-clipboard-minus'>{{ $peminjaman->count() }}</x-card>
+    <x-card cardTitle='Pengembalian' icon='bi-repeat'>{{ $pengembalian->count() }}</x-card>
+
+    @if (Session::has('toastSuccess'))
+        <script>
+            toastr.success("{{ Session::get('toastSuccess') }}");
+        </script>
+    @endif
+
+    @can('admin')
         <div class="col-12">
             <div class="card">
                 <div class="card-body">
@@ -17,40 +30,10 @@
                     <canvas id="lineChart" style="max-height: 400px;"></canvas>
                     <script>
                         document.addEventListener("DOMContentLoaded", () => {
+                            const chartData = {!! json_encode($chartData) !!};
                             new Chart(document.querySelector('#lineChart'), {
                                 type: 'line',
-                                data: {
-                                    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-                                    datasets: [{
-                                            label: 'Peminjaman',
-                                            data: [65, 59, 80, 81, 56, 55, 40],
-                                            fill: false,
-                                            borderColor: 'rgb(75, 192, 192)',
-                                            tension: 0.1
-                                        },
-                                        {
-                                            label: 'Buku Masuk',
-                                            data: [20, 40, 74, 10, 34, 67, 30],
-                                            fill: false,
-                                            borderColor: 'rgb(38, 232, 35)',
-                                            tension: 0.1
-                                        },
-                                        {
-                                            label: 'Buku Rusak',
-                                            data: [20, 15, 12, 4, 8, 10, 1],
-                                            fill: false,
-                                            borderColor: 'rgb(196, 22, 54)',
-                                            tension: 0.1
-                                        }
-                                    ]
-                                },
-                                options: {
-                                    scales: {
-                                        y: {
-                                            beginAtZero: true
-                                        }
-                                    }
-                                }
+                                data: chartData,
                             });
                         });
                     </script>
@@ -61,18 +44,5 @@
             </div>
         </div><!-- End Reports -->
     @endcan
-
-    @can('user')
-        <x-card cardTitle='Peminjaman' icon='bi-clipboard-minus'>{{ $peminjaman }}</x-card>
-        <x-card cardTitle='Pengembalian' icon='bi-repeat'>{{ $pengembalian }}</x-card>
-        <x-card cardTitle='Denda' icon='bi-exclamation-circle'>{{ $denda }}</x-card>
-        <x-card cardTitle='Buku' icon='bi-book'>{{ $buku }}</x-card>
-    @endcan
-
-    @if (Session::has('toastSuccess'))
-        <script>
-            toastr.success("{{ Session::get('toastSuccess') }}");
-        </script>
-    @endif
 
 </x-layouts.main>
